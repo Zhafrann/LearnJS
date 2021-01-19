2f0c4b99da61a62e1b3095fe45c2bfb80c654edc
Zhafran Ammar <ultrazhafran@gmail.com>
2021-01-19 10:47:32 +0700

    10.45

--- basicjs.js
+++ basicjs.js
@@ -1,1 +1,47 @@
-console.log("Hello World")
+console.log("Hello World"); console.log("Hello World 2");  //statement (pakai (;) semicolon jika ingin 1 baris 2 statement)
+
+console.log("Hello World 3") // baris 3 dan 4 adalah 1 statement, atau jika statement ke baris lain maka tanpa (;) semicolon
+console.log("Hello World 4")
+
+// satu statement multi baris
+console
+    .log("Hello World 5")
+
+/* Jika statement belum selesai, jangan diberi (;) semicolon
+ console;
+     .log("Hello World")
+ */
+
+let pesan
+pesan = "Halo Kamu"
+console.log(pesan)
+pesan = "Halo Halo"
+console.log(pesan)
+
+let namaPertama = "Kevin"
+let namaKedua = "Mawar"
+namaPertama = namaKedua //variabel dapat diisikan ke variable lain
+console.log(namaPertama);
+console.log(namaKedua);
+
+let nama = "Zhafran",
+    umur = "17",
+    tempatTinggal = "Sleman"
+console.log(nama, umur, tempatTinggal);
+console.log(nama);
+console.log(umur);
+console.log(tempatTinggal);
+
+//let = misalkan (dalam matematika)
+
+//cons =  nilai tetap yang tidak dapat berubah waktu pertama kali diisi
+/*
+const namaSaya = "Zhafrannn"
+namaSaya = "Kevin" 
+*/ //akan eror
+
+const namaSaya = "Zhafrannn"
+console.log(namaSaya);
+
+const MAIN_COLOR = "#FFFFFF"
+
