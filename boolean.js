16ad0b38b62c3eebdfdceabedc797beaca4a0f7f
Zhafran Ammar <ultrazhafran@gmail.com>
2021-01-19 11:33:19 +0700

    11.33

--- boolean.js
+++ boolean.js
@@ -1,2 +1,28 @@
-const boolean = true
+// true = 1
+// false = 2
+
+const boolean = false
 console.log(boolean);
+
+const number = 1
+console.log(boolean + number); // 1 + 1 = 2
+
+if (boolean) {
+    console.log('boolean is true');
+} else {
+    console.log('boolean is false');
+}
+
+const isValid = 1 == 1
+console.log(isValid);
+
+const isMoreThan = 1 > 2
+console.log(isMoreThan);
+
+const emptyString = ''  //pada js konsep falsing = false, 0, "" (string kosong), null (ada isinya tp tanpa nilai),  undefined
+if (emptyString) {
+    console.log(1);
+} else {
+    console.log(2);
+}
+
